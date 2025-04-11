0d179a3b03ccf8ef07e9afe79d648fe55dbb450a
meadowilla <nguyen8a1yp@gmail.com>
2025-04-12 00:10:13 +0700

    update cors

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const app = express();
 dotenv.config();
 const port = process.env.PORT || 3001;
 
-app.use(cors({origin: `http://localhost:${port}`, credentials: true}));
+app.use(cors({origin: `http://localhost:5173`, credentials: true}));
 app.use(cookieParser());
 app.use(express.json());
 
@@ -21,7 +21,7 @@ app.listen(port, () => {
 module.exports = app;
 
 const mongoose = require("mongoose");
-const queryUsers = process.env.MONGODB_USERS_URI;
+const queryUsers = process.env.MONGODB_UScorERS_URI;
 
 //configure mongoose
 const userDB = mongoose.connect(queryUsers, {
