011ae4a9f05fa0645fb51e078d2e2944225619d8
Ngoc <ngocnq@sphinxjsc>
2025-05-20 12:33:08 +0700

    check

--- calculations/Flask.py
+++ calculations/Flask.py
@@ -20,42 +20,4 @@ def calculate_tax():
 
 if __name__ == "__main__":
     app.run(debug=True)
-{
-  "year":2025,
-  "month":1,
-  "dependents": 1,
-  "region": 1,2,3,4 ,
 
-  "income_labor_contract": 20000000,
-  "taxed_labor_contract": false,
-
-  "income_no_contract": 5000000,
-  "taxed_no_contract": true,
-
-  "income_foreign_contract": 30000000,
-  "deducted_tax_abroad": false,
-
-  "use_flat_rate": true,
-  "business_income_flat": {
-    "distribution": 100000000,
-    "service": 50000000
-  },
-
-  "once_off_income": {
-    "real_estate": 300000000,
-    "investment": 20000000,
-    "capital_transfer": 0,
-    "royalty":0,
-    "lottery":0,
-    "inheritance":0
-    
-  },
-  "taxed_once_off": {
-    "investment": true,
-    "real_estate": false
-    "capital_transfer": false,
-    "royalty":false,
-    "lottery":false,
-    "inheritance":false
-  }
-}
